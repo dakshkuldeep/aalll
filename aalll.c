f7b306a402f23cc3c3e7db0dbcd967e212da85d6
daksh kuldeep <dakshkuldeep125@gmail.com>
2025-08-10 21:59:44 +0530

    library ready!
    okay that was quick. much easier than i expected
    next up doccumentation.. i guess...

--- aalll.c
+++ aalll.c
@@ -1,31 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct ll_node {
-    int value;
-    struct ll_node *nextnode;
-} node;
-
-void ll_display(node *head_node);
-node *insert_at_tail(node *head_node, int value);
-node *insert_at_head(node *head_node, int value);
-node *delete_at_head(node *head_node);
-node *delete_at_end(node *head_node);
-int ll_length(node *head_node);
-int ll_search(node *head_node, int search_value);
-int ll_count(node *head_node, int search_value);
-void ll_replace(node *head_node, int search_value, int replace_value, int scope);
-node *delete_first_match(node *head_node, int search_value, int *delete_status);
-node *delete_all_match(node *head_node, int search_value, int *delete_status);
-node *ll_append(node *ll1, node *ll2);
-node *ll_reverse(node *head_node);
-void ll_sort(node *head_node);
-node *ll_clear(node *head_node);
-
-int main() {
-
-    return 0;
-}
+#include "aalll.h"
 
 void ll_display(node *head_node) {
     while(head_node != NULL) {
